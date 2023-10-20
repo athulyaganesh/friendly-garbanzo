00b05e929ca40ceb0d4c39cc4d51a55d88c9636d
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-10-20 16:10:01 -0400

    card

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div style={{ width: '100%', height: '352px' }}>
+        <iframe
+          title="TS"
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/track/79uDOz0zuuWS7HWxzMmTa2"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowFullScreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+  );
+}
+
+export default App;
+
+
